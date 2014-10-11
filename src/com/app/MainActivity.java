d0f091de7c3edbafc33162aa7a8a486ba421a470
kufd <kosinski@ukr.net>
2014-10-11 23:47:30 +0300

    change directory structure

---
+++ src/com/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity 
+{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+	    setContentView(R.layout.form_add_spending);
+	}
+}
